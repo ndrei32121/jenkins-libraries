39c89555fe75695269f20763e3ee8a8f0b1242f0
Andrei Lazarev <and.blkv@gmail.com>
2022-02-10 14:35:35 +0300

    t

--- vars/runLinuxScript.groovy
+++ vars/runLinuxScript.groovy
@@ -2,3 +2,10 @@ def call(Map arguments = [:]) {
     loadLinuxScript(scriptName: "${arguments.scriptName}")
     sh "bash ./${arguments.scriptName}"
 }
+
+
+def loadLinuxScript(Map args = [:]) {
+    def content = libraryResource "linux/scripts/${args.scriptName}"
+    writeFile file: "${args.scriptName}", text: content
+    sh "chmod +x ${args.scriptName}" 
+}
