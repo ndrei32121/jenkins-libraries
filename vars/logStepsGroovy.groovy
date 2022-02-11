92c506ca732c59847f80a94ebc9d7f1800247449
Andrei Lazarev <and.blkv@gmail.com>
2022-02-11 17:23:27 +0300

    d

--- vars/logStepsGroovy.groovy
+++ vars/logStepsGroovy.groovy
@@ -2,7 +2,7 @@ def call(Closure body) {
     def logs = []
     logs << "Done with tests"
     body()
-    sh(script: "touch ./logFileGroovy.log && chmod ug+rw ./logFileGroovy.log")
+    sh(script: "touch ./logFileGroovy.log && chmod ug+rw ./logFileGroovy.log && echo $pwd")
     File logFile = new File("./logFileGroovy.log")
     logs.each { logFile.append("$it\n")}
 }
