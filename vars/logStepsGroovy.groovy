98b86b69f4b6fd7b2e61a1af76ef6c373fc454de
Andrei Lazarev <and.blkv@gmail.com>
2022-02-25 18:23:16 +0300

    1

--- vars/logStepsGroovy.groovy
+++ vars/logStepsGroovy.groovy
@@ -1,6 +1,7 @@
 def call(String log, Closure body) {
     body()
-    if (fileExists 'logGroovyFile.log') {
+    def exists = fileExists 'logGroovyFile.log'
+    if (exists) {
         def filecontent = readFile 'logGroovyFile.log'
     } else {
         def filecontent = ''
