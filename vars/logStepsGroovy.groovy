ef4a56bfaa1fa0f87190793078733a03f0b1a055
Andrei Lazarev <and.blkv@gmail.com>
2022-02-25 18:39:29 +0300

    test

--- vars/logStepsGroovy.groovy
+++ vars/logStepsGroovy.groovy
@@ -1,6 +1,6 @@
 def call(String log, Closure body) {
     body()
     def exists = fileExists 'logGroovyFile.log'
-    def filecontent = exists? readFile 'logGroovyFile.log' : ''
+    def filecontent = exists? readFile('logGroovyFile.log') : ''
     writeFile(file: 'logGroovyFile.log', text: filecontent+"\n${log}")
 }
