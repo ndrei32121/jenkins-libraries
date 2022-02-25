17080c261921f4626837f24f0abfb0c0613c3ca7
and <and@example.com>
2022-02-25 18:00:40 +0000

    1

--- vars/logStepsGroovy.groovy
+++ vars/logStepsGroovy.groovy
@@ -1,5 +1,5 @@
 def call(String log, Closure body) {
-    def log = body()
+    log = body()
     def exists = fileExists 'logGroovyFile.log'
     def filecontent = exists? readFile('logGroovyFile.log') : ''
     writeFile(file: 'logGroovyFile.log', text: filecontent+"\n${log}")
